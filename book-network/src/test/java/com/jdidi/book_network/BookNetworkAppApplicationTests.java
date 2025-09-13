60781d713e65fe3e4a74aafdf504c16eb764cd0b
deviona880 <deviona880@gmail.com>
2025-09-13 23:18:01 +0100

    first commit

---
+++ book-network/src/test/java/com/jdidi/book_network/BookNetworkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdidi.book_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
