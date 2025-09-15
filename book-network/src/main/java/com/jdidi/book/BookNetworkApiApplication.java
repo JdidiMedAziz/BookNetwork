5a2c97c3607d1196e3a8d6da7fd2d27779118ddc
deviona880 <deviona880@gmail.com>
2025-09-15 18:23:36 +0100

    Docker Configuration + Error fix

---
+++ book-network/src/main/java/com/jdidi/book/BookNetworkApiApplication.java
@@ -0,0 +1,15 @@
+package com.jdidi.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+
+		System.out.println("aaaaaaaaa");
+	}
+
+}
---
+++ book-network/src/test/java/com/jdidi/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdidi.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
