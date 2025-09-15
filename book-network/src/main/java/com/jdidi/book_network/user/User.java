330cd9143b5abc048e4322b21e0f0a84b033fcc0
deviona880 <deviona880@gmail.com>
2025-09-15 23:25:32 +0100

    Role Entity

--- book-network/src/main/java/com/jdidi/book_network/user/User.java
+++ book-network/src/main/java/com/jdidi/book_network/user/User.java
@@ -1,5 +1,6 @@
 package com.jdidi.book_network.user;
 
+import com.jdidi.book_network.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -7,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -14,6 +16,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Getter
@@ -45,8 +48,9 @@ public class User implements UserDetails, Principal {
     @Column(insertable = false)
     private LocalDateTime LastModifiedDate;
 
-    //private List<Role> roles;
-    
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
 
 
     @Override
@@ -56,7 +60,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream()
+                .map(r-> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
